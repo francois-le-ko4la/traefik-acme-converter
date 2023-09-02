e2d65f944972869c2551a6a39ffece08dc8abc00
Anthony Dardano <anthony.dardano@outlook.com>
2023-09-02 13:47:13 -0400

    Simplify script; Loop through all domains

--- watch-and-extract.sh
+++ watch-and-extract.sh
@@ -2,38 +2,47 @@
 
 # Variables with default values
 WATCH_DIR="${WATCH_DIR:-/traefik/certs}"
-INTERVAL="${INTERVAL:-600}"
-PROVIDER_PATH="${PROVIDER_PATH:-.cloudflare.Certificates[]}"
-DOMAIN="${DOMAIN:-www.example.com}"
-ACME_FILE_NAME="${ACME_FILE_NAME:-ACME.json}"
+INTERVAL="${INTERVAL:-1800}"
+PROVIDER="${PROVIDER:-ACME}"
+ACME_FILE_NAME=${PROVIDER}.json
 OUTPUT_DIR="${OUTPUT_DIR:-/app/output}"
 
 # Loop indefinitely
 while true; do
 
   # Check if the ACME.json file exists in the watch directory
-  echo "Checking if \"${WATCH_DIR}/${ACME_FILE_NAME}\" exists..." 
+  echo "Checking if \"${WATCH_DIR}/${ACME_FILE_NAME}\" exists..."
   if [ -f "${WATCH_DIR}/${ACME_FILE_NAME}" ]; then
 
     echo "\"${WATCH_DIR}/${ACME_FILE_NAME}\" found!"
 
-    echo "Extracting the fullchain and privkey using jq"
-    FULLCHAIN=$(jq -r "${PROVIDER_PATH} | select(.domain.main==\"${DOMAIN}\").certificate" "${WATCH_DIR}/${ACME_FILE_NAME}")
-    PRIVKEY=$(jq -r "${PROVIDER_PATH} | select(.domain.main==\"${DOMAIN}\").key" "${WATCH_DIR}/${ACME_FILE_NAME}")
+    # Getting the length of the \"Certificates\" array for ${PROVIDER}..."
+    CERTIFICATE_COUNT=$(jq -r ".${PROVIDER}.Certificates | length" "${WATCH_DIR}/${ACME_FILE_NAME}")
 
-    echo "Checking if jq returned anything"
-    if [ -z "$FULLCHAIN" ] || [ -z "$PRIVKEY" ]; then
-      echo "Certificate for domain ${DOMAIN} not found. Please try another domain name..."
-      echo "Skipping..."
-    else
-      echo "Creating output directory if it doesn't exist..."
-      mkdir -p "${OUTPUT_DIR}/${DOMAIN}"
+    echo "Looping through all the domains..."
+    for i in $(seq 0 $(($CERTIFICATE_COUNT -1))); do
+      DOMAIN=$(jq -r ".${PROVIDER}.Certificates[$i].domain.main" "${WATCH_DIR}/${ACME_FILE_NAME}")
+      FULLCHAIN=$(jq -r ".${PROVIDER}.Certificates[$i].certificate" "${WATCH_DIR}/${ACME_FILE_NAME}")
+      PRIVKEY=$(jq -r ".${PROVIDER}.Certificates[$i].key" "${WATCH_DIR}/${ACME_FILE_NAME}")
 
-      echo "Saving the fullchain and privkey to files..."
-      echo "$FULLCHAIN" | base64 -d > "${OUTPUT_DIR}/${DOMAIN}/fullchain.pem"
-      echo "$PRIVKEY" | base64 -d > "${OUTPUT_DIR}/${DOMAIN}/privkey.pem"
-      echo "Certificates for ${DOMAIN} have been extracted."
-    fi
+      # Checking if jq returned anything"
+      if [ -z "$FULLCHAIN" ] || [ -z "$PRIVKEY" ]; then
+        echo "Certificate for domain ${DOMAIN} not found. Please try another domain name..."
+        echo "Skipping..."
+      else
+        # Creating output directory if it doesn't exist...
+        mkdir -p "${OUTPUT_DIR}/${DOMAIN}"
+
+        # Saving the fullchain and privkey to files...
+        echo "$FULLCHAIN" | base64 -d > "${OUTPUT_DIR}/${DOMAIN}/fullchain.pem"
+        echo "$PRIVKEY" | base64 -d > "${OUTPUT_DIR}/${DOMAIN}/privkey.pem"
+
+        # Setting the appropriate file permissions
+        chmod 600 ${OUTPUT_DIR}/${DOMAIN}/*
+
+        echo "Certificates for ${DOMAIN} have been extracted."
+      fi
+    done
   else
     echo "File ${WATCH_DIR}/${ACME_FILE_NAME} not found. Skipping..."
   fi
